6fcd415c0b4ae6cb85f9df1cfa2834ba13c8fdf9
Malki Groosman <malkigr@gmail.com>
2015-06-07 13:08:03 +0300

    Add setter and getter method to Test class

--- Othello/Othello/src/Test.java
+++ Othello/Othello/src/Test.java
@@ -1,6 +1,8 @@
 
 public class Test {
-    int t;
+    private int t;
 	public Test(int t){this.t=t;}
 	public String toString(){return "t="+t;}
+	public void sett(int t) {this.t=t;}
+	public int gett(){return t;}
 }
