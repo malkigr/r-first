7bfb7968320974fa3a7bc65ca1c2bbfa9a6eb308
Malki Groosman <malkigr@gmail.com>
2015-06-07 12:51:08 +0300

    Add instance of Test IN App class

--- Othello/Othello/src/App.java
+++ Othello/Othello/src/App.java
@@ -6,12 +6,16 @@
 		public class App
 		{
 		   public static void main( String args[] )
-		   { 
+		   {  Test t = new Test(1);
+		      String s=t.toString();
+		      s=s+"just testing";
+		   
 		      // Sample application 
 			  System.out.println("Java application");
 			  BoardFrame board = new BoardFrame();
 		      board.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 		      board.setSize( 600,600); // set frame size
 		      board.setVisible( true ); // display frame
+		      
 		   } // end main
 		} // end class TextFieldTest
